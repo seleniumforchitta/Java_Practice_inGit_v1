fe4c3c9c1342d1e929b5d817e851963d7e9ff56b
seleniumforchitta <seleniumforchitta@gmail.com>
2023-09-03 06:31:08 +0530

    Updating Project

--- src/CollectionPrograms/ArrayListPractice.java
+++ src/CollectionPrograms/ArrayListPractice.java
@@ -6,8 +6,8 @@ public class ArrayListPractice {
 
 	public static void main(String[] args) {
 
-		List a = new ArrayList();
-		ArrayList b = new ArrayList();
+		List<String> a = new ArrayList<String>();
+		ArrayList<String> b = new ArrayList<String>();
 		
 		a.add("Bapu");
 		a.add("Bapi");
@@ -16,14 +16,14 @@ public class ArrayListPractice {
 		
 		
 		System.out.println(a);
-		b=(ArrayList) a;
+		b=(ArrayList<String>) a;
 		System.out.println(b);
 		
 		a.remove(1);
 		a.remove("Biswa");
 		a.add(2, "Chitta_2");
 		
-		Iterator it = a.iterator();
+		Iterator<String> it = a.iterator();
 		while(it.hasNext())
 		{
 			System.out.println(it.next());
